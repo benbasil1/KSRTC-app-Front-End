7e937ee5a623a154827fe13fbd8a2d424e277add
benbasil1 <benbasilreji6@gmail.com>
2024-04-08 23:44:25 +0530

    Add bus completed

---
+++ src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                  <div class="container-fluid">
+                    <a class="navbar-brand" href="#">KSRTC</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                      <ul class="navbar-nav">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="/">Add Bus</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">View All Bus</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Delete Bus</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Search</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
